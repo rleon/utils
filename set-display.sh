28799078bcd4792fe5aac1e96f6202505fc67509
Leon Romanovsky <leon.romanovsky@sandisk.com>
2014-03-06 09:15:02 +0200

    Udev support script to hotplug display change
    This script is part of udev rule to hotplug external monitor.
    My setup is pretty static and can be one of the following situations:
    1. Notebook panel only.
    2. Notebook panel and projector connected to VGA.
    3. Docking station with two monitors and notebook panel powered off.
    
    Signed-off-by: Leon Romanovsky <leon@leon.nu>

---
+++ set-display.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# This script is intended to set display according to 
+# preset configuration.
+# My setup includes one of the following setups:
+# * Notebook display only, no external displays - LVDS1 connected.
+# * Notebook display + one external display which presents the same 
+#	output - LVDS1 connected and VGA1 or HDMI1 connected.
+# * Two external displays - VGA1 and HDMI1 connected, LVDS1 is connected too but powered off.
+
+export DISPLAY=:0
+export XAUTHORITY=/home/leon.romanovsky/.Xauthority
+
+NUMB_OF_DISPLAYS=$(xrandr -q | grep " connected" | awk '{print $1}' | wc -l)
+
+case "$NUMB_OF_DISPLAYS" in
+1) xrandr --output HDMI3 --off 
+   xrandr --output LVDS1 --mode 1600x900
+   xrandr --output VGA1 --off
+;;
+2) xrandr --output VGA1 --auto
+;;
+3) xrandr --output VGA1 --mode 1920x1080 
+   xrandr --output LVDS1 --off 
+   xrandr --output HDMI3 --mode 1680x1050 --left-of VGA1 
+   xrandr --output HDMI3 --primary
+;;
+esac
