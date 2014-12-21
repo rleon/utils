8e9dfc8161ae6c60ff5fbdb1571254045bc84e80
Leon Romanovsky <leon.romanovsky@sandisk.com>
2014-12-21 15:40:25 +0200

    change my docking stationsetup
    Signed-off-by: Leon Romanovsky <leon.romanovsky@sandisk.com>

--- set-display.sh
+++ set-display.sh
@@ -8,20 +8,22 @@
 # * Two external displays - VGA1 and HDMI1 connected, LVDS1 is connected too but powered off.
 
 export DISPLAY=:0
-export XAUTHORITY=/home/leon.romanovsky/.Xauthority
+export XAUTHORITY=/home/leon/.Xauthority
 
 NUMB_OF_DISPLAYS=$(xrandr -q | grep " connected" | awk '{print $1}' | wc -l)
 
 case "$NUMB_OF_DISPLAYS" in
-1) xrandr --output HDMI3 --off 
+1) xrandr --output HDMI3 --off
    xrandr --output LVDS1 --mode 1600x900
+   xrandr --output HDMI2 --off
    xrandr --output VGA1 --off
 ;;
 2) xrandr --output VGA1 --auto
 ;;
-3) xrandr --output VGA1 --mode 1920x1080 
-   xrandr --output LVDS1 --off 
-   xrandr --output HDMI3 --mode 1680x1050 --left-of VGA1 
+3) xrandr --output VGA1 --off
+   xrandr --output HDMI2 --mode 1920x1080
+   xrandr --output LVDS1 --off
+   xrandr --output HDMI3 --mode 1920x1080 --left-of HDMI2
    xrandr --output HDMI3 --primary
 ;;
 esac
